97194b45b4a4a36aaf7dd8aff72cdb829c5c952e
francoism <franck.martin3d@gmail.com>
2019-03-05 16:57:07 +1100

    All class for project

---
+++ TowerDefence/TowerDefence/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefence
+{
+    class Game
+    {
+        static void Main()
+        {
+            // Initiate a new Tower object 
+            Tower tower = new Tower();
+        }
+    }
+}
